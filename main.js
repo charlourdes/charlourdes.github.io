ed6b41822c3a0015386fddb3db7f1a26ff310c2c
charlourdes <116295537+charlourdes@users.noreply.github.com>
2024-03-02 23:25:55 +0000

    icons and menu updates

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-console.log("Hello World!");
+console.log('JavaScript file loaded!');
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all the menu links
+    const menuLinks = document.querySelectorAll('.menu ul li a');
 
-
+    // Loop through each menu link
+    menuLinks.forEach(link => {
+        // Get the width of the menu link text
+        const linkWidth = link.offsetWidth;
+        // Set the width of the hover effect to the width of the menu link text plus some padding
+        link.style.setProperty('--link-width', `${linkWidth}px`);
+    });
+});
